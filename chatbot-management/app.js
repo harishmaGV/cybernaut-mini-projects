d98f18c3c721e51644375ecabcaa666b7530732b
harishmaGV <156167870+harishmaGV@users.noreply.github.com>
2025-03-15 11:12:38 +0530

    Create app.js

---
+++ chatbot-management/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatbotSystem from './components/chatbot/ChatbotSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatbotSystem />
+    </div>
+  );
+}
+
+export default App;
